3e894be87cde0fd52f2ed6e822fc3b3a29bc13d3
Lee, Geonwoo <97107952+ge429@users.noreply.github.com>
2022-01-04 23:23:29 +0900

    1월 4일 푼 문제

---
+++ 10757.py
@@ -0,0 +1,4 @@
+arr = input().split()
+a = arr[0]
+b = arr[1]
+print(int(a)+int(b))
---
+++ 10869.py
@@ -0,0 +1,8 @@
+arr = input().split()
+a = arr[0]
+b = arr[1]
+print(int(a)+int(b))
+print(int(a)-int(b))
+print(int(a)*int(b))
+print(int(a)//int(b))
+print(int(a)%int(b))
